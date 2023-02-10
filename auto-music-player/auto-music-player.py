44861839afe55cf4263ef0431110244ffa277714
lucifer-14 <killerlucifer14@gmail.com>
2023-02-10 21:42:35 +0630

    Final edit for db based auto music player

--- auto-music-player/auto-music-player.py
+++ auto-music-player/auto-music-player.py
@@ -9,16 +9,31 @@ def requirement_check():
         check = False
     return check
 
+def file_check(path: str):
+    check = True
+    if not os.path.isfile(path):
+        check = False
+    return check
+
+
+def play_music(song: tuple):
+    music_player = pgmixer.music
+    pgmixer.init()
+    music_player.load(song[1])
+    music_player.set_volume(0.1)
+    music_player.play()
+    print(f"\n[+] Playing song: {song[0]}\n")
+
 
 def auto_music_player():
     
     if requirement_check():
+        columns = ['SONG_NAME', 'PATH', 'AUTHOR', 'ALBUM', 'IS_FAVOURITE']
+        songs_list = dc.extract_data_from_table(table="SONGS", columns=columns)
+        for song in songs_list:
+            if file_check(song[1]):
+                play_music(song)
 
-        music_player = pgmixer.music
-        music_player.init()
-        music_player.load()
-        music_player.set_volume(0.1)
-        music_player.play()
 
 
 if __name__ == "__main__":

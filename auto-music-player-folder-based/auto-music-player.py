b9564ef0d47debf0de34fc65ed799c7c667662f1
lucifer-14 <killerlucifer14@gmail.com>
2023-02-11 02:54:55 +0630

    added pause, unpause, play, add, remove queue, set, get volume

--- auto-music-player-folder-based/auto-music-player.py
+++ auto-music-player-folder-based/auto-music-player.py
@@ -1,13 +1,18 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame.mixer as pgmixer
+import threading
+import time
 
 SONGS_FOLDER = "songs"          # You can configure it yourself
 
-QUEUE = ['abc','def']
+QUEUE = []                      
+HAS_STARTED = True              # Don't do anything here
+COUNTER = 0
 
 MUSIC_PLAYER = pgmixer.music    # Don't do anything here
 pgmixer.init()
+MUSIC_PLAYER.set_volume(0.1)
 
 def requirement_check():
     global SONGS_FOLDER
@@ -32,27 +37,6 @@ def play_music(song: str):
     print(f"\n[+] Playing song: {os.path.basename(song)}\n")
 
 
-def auto_music_player():
-    global SONGS_FOLDER
-    if requirement_check():
-        data_list = os.listdir(SONGS_FOLDER)
-        for data in data_list:
-            if os.path.isfile(os.path.join(SONGS_FOLDER, data)):
-                if data.endswith == ".mp4":
-                    play_music(os.path.join(SONGS_FOLDER, data))
-            else:
-                songs_list = os.listdir(os.path.join(SONGS_FOLDER, data))
-                for song in songs_list:
-                    if os.path.isfile(song):
-                        if song.endswith == ".mp4":
-                            play_music(song)
-        # columns = ['SONG_NAME', 'PATH', 'AUTHOR', 'ALBUM', 'IS_FAVOURITE']
-        # songs_list = dc.extract_data_from_table(table="SONGS", columns=columns)
-        # for song in songs_list:
-        #     if file_check(song[1]):
-        #         play_music(song)
-        pass
-
 def help_menu(cmd):
     pass
 
@@ -63,6 +47,15 @@ def set_controls(cmd):
     if option.lower() == "volume":
         MUSIC_PLAYER.set_volume(int(value)/100)
         print(f"\n[+] Volume set to {value}.\n")
+
+def get_controls(cmd):
+    global MUSIC_PLAYER
+    split_cmd = cmd.split(" ")
+    option = split_cmd[1].lower()
+    # value = " ".join(split_cmd[2:])
+    if option == "volume":
+        volume = MUSIC_PLAYER.get_volume()
+        print(f"[+] Current Volume {volume*100}")
 
 
 
@@ -169,7 +162,10 @@ def show_command(cmd):
         print("=====")
         for i, song in enumerate(QUEUE):
             is_queue_empty = False
-            print(f"{str(i)}) {os.path.basename(song)}")
+            if i == COUNTER:
+                print(f"{str(i)}) {os.path.basename(song)} <=")
+            else:
+                print(f"{str(i)}) {os.path.basename(song)}")
 
         if is_queue_empty:
             print("\n*Empty*")
@@ -183,38 +179,157 @@ def show_command(cmd):
     # print(option)
 
 
-def play_command(cmd):
+def queue_command(cmd):
+    global QUEUE
+    global HAS_STARTED
+    global SONGS_FOLDER
+    global COUNTER
     split_cmd = cmd.split(" ")
-    
+    main_cmd = split_cmd[0].lower()
+    option = split_cmd[1].lower()
+    value = " ".join(split_cmd[2:])
+    if main_cmd == "add":
+
+        if option == "playlist":
+            if value:
+                if os.path.exists(os.path.join(SONGS_FOLDER, value)):
+                    data_list = os.listdir(os.path.join(SONGS_FOLDER, value))
+                    if data_list != []:
+                        print(f"\n[+] Songs added -")
+                        for data in data_list:
+                            QUEUE.append(os.path.join(SONGS_FOLDER, value, data))
+                            print(f"{os.path.basename(data)}")
+                        print()
+                    else:
+                        print(f"\n[+] Empty Playlist. No Songs Added.\n")
+
+                else:
+                    print(f"\n[-] Cannot Find the Playlist: {value}\n")
+            else:
+                print("\n[-] Error. Need to Specify Playlist Name.\n")
+        elif option == "song":
+            if value:
+                if os.path.isfile(os.path.join(SONGS_FOLDER, value)):
+                    QUEUE.append(os.path.join(SONGS_FOLDER, value))
+                    print(f"\n[+] Songs added - {os.path.basename(value)}\n")
+                else:
+                    print(f"\n[-] Cannot Find the Song: {value}\n")
+            else:
+                print("\n[-] Error. Need to Specify Song Name.\n")
+
+    elif main_cmd == "remove":
+
+        if option == "all":
+            QUEUE.clear()
+            print(f"\n[-] Removed All Songs from Queue.\n")
+        else:
+            try:
+                tmp = QUEUE[int(option)]
+                QUEUE.pop(int(option))
+                if int(option) <= COUNTER:
+                    COUNTER -= 1
+                print(f"\n[-] Removed `{os.path.basename(tmp)}` Song from Queue.\n")
+            except IndexError:
+                print(f"\n[-] Index Error. Total Songs in Queue: {len(QUEUE)}\n")
+
+        if QUEUE == []:
+            HAS_STARTED = True
+    # if QUEUE == []:
+    #     HAS_STARTED = False
+    # else:
+    #     HAS_STARTED = True
+    if HAS_STARTED:
+        # if QUEUE != []:
+            t1 = threading.Thread(target=auto_music_player)
+            HAS_STARTED = False
+            t1.start()
+            
 
 
 def player_controls(cmd):
-    pass
+    global MUSIC_PLAYER
+    split_cmd = cmd.split(" ")
+    main_cmd = split_cmd[0].lower()
+    # option = split_cmd[1].lower()
+    # value = " ".join(split_cmd[2:])
+    if main_cmd == "pause":
+        MUSIC_PLAYER.pause()
+    elif main_cmd == "unpause":
+        MUSIC_PLAYER.unpause()
 
+
+def auto_music_player():
+    global HAS_STARTED
+    global QUEUE
+    global COUNTER
+    if QUEUE == []:
+        HAS_STARTED = True
+        # print("true")
+    else:
+        while COUNTER < len(QUEUE):
+            MUSIC_PLAYER.load(QUEUE[COUNTER])
+            MUSIC_PLAYER.play()
+            print(f"\n[+] Playing song: {os.path.basename(QUEUE[COUNTER])}\n")
+            while MUSIC_PLAYER.get_busy():
+                time.sleep(2)
+                # print('playing')
+            COUNTER += 1
+            if QUEUE == []:
+                break
+            # print(QUEUE)
+            
+        # print('ok')
+
+def auto_music_player_old_v():
+    global SONGS_FOLDER
+    if requirement_check():
+        data_list = os.listdir(SONGS_FOLDER)
+        for data in data_list:
+            if os.path.isfile(os.path.join(SONGS_FOLDER, data)):
+                if data.endswith == ".mp4":
+                    play_music(os.path.join(SONGS_FOLDER, data))
+            else:
+                songs_list = os.listdir(os.path.join(SONGS_FOLDER, data))
+                for song in songs_list:
+                    if os.path.isfile(song):
+                        if song.endswith == ".mp4":
+                            play_music(song)
+        # columns = ['SONG_NAME', 'PATH', 'AUTHOR', 'ALBUM', 'IS_FAVOURITE']
+        # songs_list = dc.extract_data_from_table(table="SONGS", columns=columns)
+        # for song in songs_list:
+        #     if file_check(song[1]):
+        #         play_music(song)
+        pass
 
 if __name__ == "__main__":
-    
     print("\nAUTO MUSIC PLAYER")
     print("=================\n")
     while True:
         try:
             cmd = input("cmd> ")
             main_cmd = cmd.split(" ")[0].lower()
-            if main_cmd == "help":
+            if cmd == "":
+                pass
+            elif main_cmd == "help":
                 help_menu(cmd)
             elif main_cmd == "set":
                 set_controls(cmd)
+            elif main_cmd == "get":
+                get_controls(cmd)
             elif main_cmd == "show":
                 show_command(cmd)
-            elif main_cmd == "play":
-                play_command(cmd)
+            elif main_cmd == "add" or main_cmd == "remove":
+                queue_command(cmd)
             elif main_cmd == "next" or main_cmd == "back" or main_cmd == "pause" or main_cmd == "unpause" or main_cmd == "stop":
                 player_controls(cmd)
             else:
-                print('Try `help` for more information. Ctrl+C to quit program.')
+                print('Try `help` for more information. Ctrl+C or `exit` to quit program.')
         except KeyboardInterrupt:
+            QUEUE = []
+            MUSIC_PLAYER.stop()
+            # t1.join()
             print("\n\n[+] Auto Music Player terminated.\n")
             break
 
-    auto_music_player()
+    # auto_music_player()
     

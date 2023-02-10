00cb47d6aab26e0432cfa15912b5375265a13be2
lucifer-14 <killerlucifer14@gmail.com>
2023-02-10 20:23:39 +0630

    Added extract data from table function in db connection

--- auto-music-player/database_connection.py
+++ auto-music-player/database_connection.py
@@ -13,9 +13,9 @@ def create_table(table: str = "", columns: list = [], query: str = ""):
             query += col + ", "
         query = query[:len(query)-2] + ")"
 
-        # print(query)                                              # uncomment to debug
+        # print(query)                                                # uncomment to debug
     CONN.execute(query)
-    #print("\n[+] Successfully added a table.\n")                    # uncomment to show log
+    # print("\n[+] Successfully added a table.\n")                    # uncomment to show log
 
 
 def insert_data_into_table(table: str = "", data_pair: list = [], query: str = ""):
@@ -31,11 +31,10 @@ def insert_data_into_table(table: str = "", data_pair: list = [], query: str = "
             query += data + ", "
         query = query[:len(query)-2] + ")"
 
-        # print(query)                                              # uncomment to debug
+        # print(query)                                               # uncomment to debug
     CONN.execute(query)
     CONN.commit()
-    #print("\n[+] Successfully added data to the table.\n")         # uncomment to show log
-
+    # print("\n[+] Successfully added data to the table.\n")         # uncomment to show log
 
 
 def extract_data_from_table(table: str = "", columns: list = [], query: str = "", cond: str = ""):
@@ -45,21 +44,45 @@ def extract_data_from_table(table: str = "", columns: list = [], query: str = ""
 
         for col in columns:
             query += col + ", "
-        query = query[:len(query)-2] + " FROM {table}"
+        query = query[:len(query)-2] + f" FROM {table}"
 
         if cond:
             query += " WHERE " + cond
 
 
-        # print(query)                                              # uncomment to debug
-    #print("\n[+] Successfully added data to the table.\n")         # uncomment to show log
+        print(query)                                               # uncomment to debug
+    # print("\n[+] Successfully added data to the table.\n")         # uncomment to show log
     cur = CONN.cursor()
     result = cur.execute(query)
-    print(result)
+    print(result.fetchall())
     return result.fetchall()
 
 
+if __name__ == "__main__":
+    # column_template = ['ID INTEGER PRIMARY KEY     AUTOINCREMENT',
+    #             'SONG_NAME           VARCHAR(50)    NOT NULL',
+    #             'AUTHOR         VARCHAR(100)',
+    #             'ALBUM          VARCHAR(50)',
+    #             'IS_FAVOURITE   BOOLEAN NOT NULL DEFAULT 0',
+    #             'PLAYLIST_ID          INTEGER']
 
+    # create_table(table = "SONGS", columns=column_template)
+
+    # column_template = ['ID INTEGER PRIMARY KEY     AUTOINCREMENT',
+    #             'PLAYLIST_NAME           VARCHAR(50)    NOT NULL',
+    #             'IS_FAVOURITE   BOOLEAN NOT NULL DEFAULT 0']
+
+    # create_table(table = "PLAYLISTS", columns=column_template)
+
+    # data_pair_template = [  ["SONG_NAME", "'testing2'"],
+    #                         ["IS_FAVOURITE", "1"]]
+
+    # insert_data_into_table(table="SONGS", data_pair=data_pair_template)
+
+    column_template = ["*", "IS_FAVOURITE"]
+    cond_template = "SONG_NAME LIKE '%in%'"
+    extract_data_from_table(table="SONGS", columns=column_template, cond=cond_template)
+    pass
 
 
 # def extract_songs_list():
